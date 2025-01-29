37dd895dfe9688cad5990c359073d918876c163a
EricDazzleG <eric.dazzle.george@gmail.com>
2025-01-29 19:27:31 +0000

    Code challenge submission for question: 11 Container With Most Water

---
+++ 11 Container With Most Water/11container-with-most-water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxarea = 0;
+        int left =0; int right = height.size()-1;
+        while(left<right){
+            int h= min(height[left],height[right]);
+            int area = h*(right-left);
+            if(area > maxarea){
+                maxarea= area;
+            }
+
+            if(height[left]>height[right]){
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+        return maxarea;
+
+    }
+};
