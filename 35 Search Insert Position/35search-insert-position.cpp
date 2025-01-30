c0010476e7cec483cdecffe55c0292a20ca054e3
EricDazzleG <eric.dazzle.george@gmail.com>
2025-01-30 19:23:24 +0000

    Code challenge submission for question: 35 Search Insert Position

---
+++ 35 Search Insert Position/35search-insert-position.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int s=0;int mid;
+        int e= nums.size()-1;
+        
+        while(s<=e){
+            mid = s + (e-s)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>target){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+            
+        }
+        if(nums[mid]>target){
+            return mid;
+        }
+        else if (nums[mid]<target){
+            return mid+1;
+        }
+
+       return{}; 
+    }
+};
