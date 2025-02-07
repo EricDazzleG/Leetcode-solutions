ab9af250642275ae726cf30bbce4edf2caa551bc
EricDazzleG <eric.dazzle.george@gmail.com>
2025-02-07 18:01:50 +0000

    Code challenge submission for question: 492 Construct the Rectangle

---
+++ 492 Construct the Rectangle/492construct-the-rectangle.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> constructRectangle(int area) {
+        int l = sqrt(area);
+        while (area % l != 0) {
+            l--;
+        }
+        return {area / l, l}; 
+
+    }
+};
