cd1aed3498ff64f659c04689628f3acd977b5af6
EricDazzleG <eric.dazzle.george@gmail.com>
2025-02-21 20:03:44 +0000

    Code challenge submission for question: 81 Search in Rotated Sorted Array II

---
+++ 81 Search in Rotated Sorted Array II/81search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int low =0;
+        sort(nums.begin(),nums.end());
+        int high =nums.size()-1;
+        while(low<=high){
+            int mid  = low + (high-low)/2;
+    
+            if(nums[mid]==target){
+                return true;
+            }
+            else if(nums[mid]>target){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return false;
+    }
+};
