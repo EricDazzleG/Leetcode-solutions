084d15bb3d0f76b4c93d11e664bfbb340254637a
EricDazzleG <eric.dazzle.george@gmail.com>
2025-07-18 08:06:42 +0000

    Code challenge submission for question: 103 Binary Tree Zigzag Level Order Traversal

---
+++ 103 Binary Tree Zigzag Level Order Traversal/103binary-tree-zigzag-level-order-traversal.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==NULL){
+            return ans;
+        }
+    
+        queue<TreeNode*> p;
+        p.push(root);
+        bool lr=true;
+        while(!p.empty()){
+            int s = p.size();
+            vector<int> v(s); 
+            for(int i =0;i<s;i++){
+                TreeNode* temp = p.front();
+                p.pop();
+                if(temp->left){
+                    p.push(temp->left);
+                }
+                if(temp->right){
+                    p.push(temp->right);
+                }
+                int index = lr? i : s-i-1;
+                v[index]=temp->val;
+                
+            }
+            lr = !lr;
+            ans.push_back(v);
+        }
+        return ans;
+    }
+};
