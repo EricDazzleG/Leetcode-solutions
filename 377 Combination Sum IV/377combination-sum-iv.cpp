93e59f00a49fa6bc5a3d126af21befb506ccca87
EricDazzleG <eric.dazzle.george@gmail.com>
2025-10-04 08:17:15 +0000

    Code challenge submission for question: 377 Combination Sum IV

---
+++ 377 Combination Sum IV/377combination-sum-iv.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int solve(vector<int>& nums,int target,vector<int>& dp){
+        if(target<0){
+            return 0;
+        }
+        if(target==0){
+            return 1;
+        }
+        if(dp[target]!=-1){
+            return dp[target];
+        }
+        int ans=0;
+        for(int i =0;i<nums.size();i++){
+            ans =ans+ solve(nums,target-nums[i],dp);
+        }
+        dp[target] = ans;
+        return dp[target];
+
+    }
+    int solve2(vector<int>& nums, int target){
+        vector<unsigned long long> dp(target+1,0);
+        dp[0]=1;
+        for(int i =1;i<=target;i++){
+            for(int j =0;j<nums.size();j++){
+                if(i-nums[j]>=0)
+                    dp[i]+=dp[i-nums[j]];
+            }
+        }
+        return dp[target]>INT_MAX ?INT_MAX: (int)dp[target];
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+       // vector<int> dp(target+1,-1);
+        //return solve(nums,target,dp);
+        return solve2(nums,target);
+    }
+};
