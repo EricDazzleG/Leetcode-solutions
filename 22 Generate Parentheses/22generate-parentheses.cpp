b58f2678dc56a97a110480c68877c73aeaaab7fd
EricDazzleG <eric.dazzle.george@gmail.com>
2026-03-30 10:27:03 +0000

    Code challenge submission for question: 22 Generate Parentheses

---
+++ 22 Generate Parentheses/22generate-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void solve(int open,int close, string s,int n, vector<string>& ans){
+        if(open +close == 2*n){
+            ans.push_back(s);
+            return;
+        }
+        if(open<n){
+            solve(open+1,close,s+"(",n,ans);
+        }
+        if(close<open){
+            solve(open,close+1,s+")",n,ans);
+        }
+
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        solve(0,0,"",n,ans);
+        return ans;
+    }
+};
