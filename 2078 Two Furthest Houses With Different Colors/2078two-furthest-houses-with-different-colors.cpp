024f03d0395a493937296014c4dd07e4f4c750e8
EricDazzleG <eric.dazzle.george@gmail.com>
2026-04-20 07:13:16 +0000

    Code challenge submission for question: 2078 Two Furthest Houses With Different Colors

---
+++ 2078 Two Furthest Houses With Different Colors/2078two-furthest-houses-with-different-colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n = colors.size();
+        int left = 0, right = 0;
+        for(int i =0;i<n;i++){
+            if(colors[i]^colors[n-1]){
+                left = i;
+                break;
+            }
+        }
+
+        for(int i =n-1;i>=0;i--){
+            if(colors[0]^colors[i]){
+                right =i;
+                break;
+            }
+        }
+        return max(n-1-left,right);
+    }
+};
