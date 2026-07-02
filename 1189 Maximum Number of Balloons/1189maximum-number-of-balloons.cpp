8505a358bc24292eb59490290be0610046680e31
EricDazzleG <eric.dazzle.george@gmail.com>
2026-07-02 18:02:49 +0000

    Code challenge submission for question: 1189 Maximum Number of Balloons

---
+++ 1189 Maximum Number of Balloons/1189maximum-number-of-balloons.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        int b=0,a=0,l=0,o=0,n=0;
+        for(char &s : text){
+            if(s=='b') b++;
+            if(s=='a') a++;
+            if(s=='l') l++;
+            if(s=='o') o++;
+            if(s=='n') n++;
+        }
+        if(b && a && l && o && n) return min({b,a,l/2,o/2,n});
+        else return 0;
+    }
+};
