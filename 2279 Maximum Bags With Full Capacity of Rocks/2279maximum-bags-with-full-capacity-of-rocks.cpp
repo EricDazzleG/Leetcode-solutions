529b48cb174c4a9152b261048926d748909fdf23
EricDazzleG <eric.dazzle.george@gmail.com>
2026-08-01 09:19:07 +0000

    Code challenge submission for question: 2279 Maximum Bags With Full Capacity of Rocks

---
+++ 2279 Maximum Bags With Full Capacity of Rocks/2279maximum-bags-with-full-capacity-of-rocks.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        int n = capacity.size();
+        vector<int> diff(n);
+        for(int i =0;i<n;i++){
+            diff[i] = capacity[i]-rocks[i];
+        }
+        sort(diff.begin(),diff.end());
+        int ans=0;
+        for(int x: diff){
+            if(x<=additionalRocks){
+                additionalRocks-=x;
+                ans++;
+            }
+            else{
+                break;
+            }
+        }
+        return ans;
+    }
+};
